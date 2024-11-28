8eeac73c1d60145c963be60c227bd5be67bfd642
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2024-11-28 19:52:26 +0530

    Assignment

--- Assignments/Class_3/App.js
+++ Assignments/Class_3/App.js
@@ -1,15 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const title = <h1 className="heading">Hello World From jsx</h1>;
+     // Create the `same element using JSX`
+    //  Create a `functional component of the same with JSX`
+   //  `Pass attribute` into the tag in `JSX`
 
-const HeadingComponent = () => {
-  return (
-    <div className="container">
-      {title}
-      <h1 className="heading">Hello World from Functional Component</h1>
+// const header = React.createElement("div", { className:"title" }, [
+//   React.createElement("h1", {}, "This is H1"),
+//   React.createElement("h2", {}, "This is H2 Tag"),
+//   React.createElement("h3", {}, "This is H3 Tag "),
+// ]);
+
+// const header = (
+//   <div className="title">
+//     <h1>This is H1</h1>
+//     <h2>This is H2</h2>
+//     <h3>This is H3</h3>
+//   </div>
+// );
+
+// const Header2 = () => (
+//   <div className="title">
+//     {header}
+//     <h1>This is H1</h1>
+//     <h2>This is H2</h2>
+//     <h3>This is H3</h3>
+//   </div>
+// );
+
+// - Create a `Header Component` from scratch using `Functional Component` with JSX
+//   - Add a `Logo on Left`
+//   - Add a `search bar in middle`
+//   - Add `User icon on right`
+//   - Add `CSS to make it look nice`
+
+const HeaderComponent = () => (
+  <div className="container">
+    <img
+      src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/400w/canva-yellow-abstract-cooking-fire-free-logo-8iEUG5R0yQ4.jpg"
+      className="image"
+    ></img>
+    <div>
+      <input type="Text" className="search" name="search"></input>
+      <button className="btn">Search</button>
     </div>
-  );
-};
+    <div className="user">
+      <img
+        src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/400w/canva-yellow-abstract-cooking-fire-free-logo-8iEUG5R0yQ4.jpg"
+        className="User"
+      ></img>
+    </div>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<HeaderComponent/>);

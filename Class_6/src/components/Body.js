1fea197da73bb73240c5314f8defa8b529ca4a59
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2024-12-03 13:40:58 +0530

    Class 6

---
+++ Class_6/src/components/Body.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const [searchText, setSearchText] = useState("");
+
+  const fetchData = async () => {
+    const data = await fetch("");
+    const json = await data.json();
+    console.log(json);
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button onClick={() => {
+            console.log(searchText)
+          }}>Search</button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ Class_6/src/components/Header.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+
+const Header = () => {
+  const [btnName,setBtnName]=useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={LOGO_URL} alt="App Logo" className="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button onClick={()=>{
+            btnName==="Login"?
+            setBtnName("Logout"):
+            setBtnName("Login")
+          }}>{btnName}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

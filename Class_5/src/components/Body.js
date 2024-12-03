8afa7ae9ed924beae7548a5c4ddd3036b139f830
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2024-12-03 09:27:29 +0530

    Class 6

---
+++ Class_5/src/components/Body.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  // * React Hook -> A normal JavaScript function which is given to us by React (or) Normal JS utility functions
+  // * useState() - Super Powerful variable
+  // * useEffect() -
+
+  // * State variable - Super Powerful variable
+  // const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  const [listOfRestaurants,setListOfRestaurants]= useState(resList);
+  // useState hooks can also be written in below form
+  // const arr= useState(resList);
+  // const listOfRestaurants= arr[0];
+  // const setListOfRestaurants=arr[1];
+
+  // * Normal JS variable
+  // let listOfRestaurantsJs = [
+  //   {
+  //     type: "restaurant",
+  //     data: {
+  //       id: "334475",
+  //       name: "KFC",
+  //       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+  //       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+  //       costForTwo: 40000,
+  //       deliveryTime: 36,
+  //       avgRating: "3.8",
+  //     },
+  //   },
+  //   {
+  //     type: "restaurant",
+  //     data: {
+  //       id: "334476",
+  //       name: "Dominos",
+  //       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+  //       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+  //       costForTwo: 40000,
+  //       deliveryTime: 36,
+  //       avgRating: "4.8",
+  //     },
+  //   },
+  //   {
+  //     type: "restaurant",
+  //     data: {
+  //       id: "334477",
+  //       name: "McDonals",
+  //       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+  //       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+  //       costForTwo: 40000,
+  //       deliveryTime: 36,
+  //       avgRating: "4.2",
+  //     },
+  //   },
+  // ];
+
+  return (
+    <div className="body">
+      {/* <div className="search-container">
+        <input type="text" placeholder="Search Food or Restaurant" />
+        <button>Search</button>
+      </div> */}
+      <div className="filter">
+        {/* <button
+          className="filter-btn"
+          onClick={() => {
+            // * Filter logic
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+
+            setListOfRestaurants(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button> */}
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList= listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {/* // * looping through the <RestaurentCard /> components Using Array.map() method */}
+
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ Class_5/src/components/RestaurantCard.js
@@ -0,0 +1,36 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const {
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
+
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/29/01c60b32-5f86-4539-b4cf-e2912278f9d4_23848.jpg"
+        alt="Biryani"
+      />
+
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
